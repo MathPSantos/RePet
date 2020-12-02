38d12b85951f61f70df16675aeabe0932ea78860
MathPSantos <matheus.psantos2016@gmail.com>
2020-12-02 09:28:37 -0300

    fix: Typograph component fixed

--- src/components/Typography/Headings/styles.js
+++ src/components/Typography/Headings/styles.js
@@ -5,7 +5,10 @@ export const Heading = styled.h1`
   color: ${({ theme, color }) => color === 'green' ? theme.colors.typography.quaternary : color === 'white' ? theme.colors.typography.tertiary : theme.colors.typography.primary};
   font-size: ${({ size }) => size === 'sm' ? '27px' : '40px'};
 
+  text-align: ${({ align }) => align ? align : 'left' };
+
   @media screen and (min-width: 720px) {
+    text-align: ${({ resAlign }) => resAlign ? resAlign : 'left'};
     font-size: ${({ size }) => size === 'sm' && '36px' };
   }
 `
--- src/components/Typography/Paragraphs/styles.js
+++ src/components/Typography/Paragraphs/styles.js
@@ -4,4 +4,10 @@ export const Paragraph = styled.p`
   font-family: ${({ theme, bold }) => bold ? theme.fonts.tertiary : theme.fonts.secondary};
   color: ${({ theme, color }) => color === 'white' ? theme.colors.typography.tertiary : theme.colors.typography.secondary};
   font-size: ${({ size }) => size === 'sm' ? '18px' : '20px'};
+
+  text-align: ${({ align }) => align ? align : 'left' };
+
+  @media screen and (min-width: 720px) {
+    text-align: ${({ resAlign }) => resAlign ? resAlign : 'left'};
+  }
 `
